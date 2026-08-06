2a76e4002d838997127af8bf1c87148a57ac207c
gaonawilfram-web <gaonawilfram@gmail.com>
2026-08-05 20:47:46 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='recvip-v1';
+const ASSETS=['./','./index.html','./videos.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>{e.respondWith(
+  fetch(e.request).catch(()=>caches.match(e.request)))});
